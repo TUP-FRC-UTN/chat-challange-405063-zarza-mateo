ec4b7bd2795374f00ebdea39b3192a246f2828a5
405063-zarza-mateo <405063@tecnicatura.frc.utn.edu.ar>
2024-08-31 00:46:44 -0300

    chat terminado

---
+++ chat/src/app/sala/sala.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MensajeUsuarioComponent } from '../mensaje-usuario/mensaje-usuario.component';
+import { CommonModule } from '@angular/common';
+
+interface Mensaje{
+  usuario : string;
+  texto : string;
+}
+
+@Component({
+  selector: 'app-sala',
+  standalone: true,
+  imports: [MensajeUsuarioComponent, CommonModule],
+  templateUrl: './sala.component.html',
+  styleUrl: './sala.component.css'
+})
+export class SalaComponent {
+mensajes : Mensaje[] = []
+
+agregarMensaje(mensaje : Mensaje){
+this.mensajes.push(mensaje);
+}
+}
